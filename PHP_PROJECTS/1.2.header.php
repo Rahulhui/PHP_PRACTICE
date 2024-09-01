164c54b8db4b95f99e5db4f887acd833efebf3d4
Rahul Hui <huir657@gmail.com>
2024-09-01 13:26:35 +0530

    Your commit message

--- PHP_PROJECTS/1.2.header.php
+++ PHP_PROJECTS/1.2.header.php
@@ -7,5 +7,6 @@
         <li><a href="#">About Us</a></li>
         <li><a href="#">Gallery</a></li>
         <li><a href="#">Contact Us</a></li>
+        <li><a href="#">privacy </a></li>
     </ul>
 </div>
